23ba88292f2779803866bd1f3493be32f0aa724c
jhgbrt <jeroen@haegebaert.com>
2023-12-04 21:58:07 +0100

    day 4 - jeroen - c#

--- day 04/Jeroen - C#/aoc.cs
+++ day 04/Jeroen - C#/aoc.cs
@@ -1,63 +1,44 @@
+using AdventOfCode;
+
 var input = File.ReadAllLines("input.txt");
-
-var items = input.Select(Card.Parse).ToImmutableArray();
-
+var regex = Regexes.CardRegex();
+var items = input.Select(s => regex.As<Card>(s)).ToImmutableArray();
 var sw = Stopwatch.StartNew();
-
 var part1 = (
     from c in items
-    let e = c.NofWinning
+    let e = c.NofWins
     where e > 0
     select Pow(2, e - 1)).Sum();
-
 var part2 = Part2();
-
 Console.WriteLine((part1, part2, sw.Elapsed));
-
 long Part2()
 {
-    var cards = items.ToList();
-    foreach (var card in cards)
+    (int nofwins, int count)[] cards = (
+        from item in items
+        let nofwins = item.NofWins
+        select (nofwins, count: 1)).ToArray();
+    for (int i = 0; i < cards.Length; i++)
     {
-        for (var c = 0; c < card.Count; c++)
+        var card = cards[i];
+        for (var c = 0; c < card.count; c++)
         {
-            foreach (var offset in Range(0, card.NofWinning))
+            foreach (var offset in Range(1, card.nofwins))
             {
-                cards[card.Id + offset].Count++;
+                cards[i + offset].count++;
             }
         }
     }
 
-    return cards.Sum(c => c.Count);
+    return cards.Sum(c => c.count);
 }
 
-class Card
+record Card(int id, int[] winning, int[] numbers)
 {
-    int id;
-    int nofwinning;
-    public Card(int id, int[] winning, int[] numbers)
-    {
-        this.id = id;
-        nofwinning = winning.Count(numbers.Contains);
-        Count = 1;
-    }
-
-    public int Count { get; set; }
-    public int Id => id;
-    public int NofWinning => nofwinning;
-
-    public static Card Parse(string s)
-    {
-        var match = Regexes.MyRegex().Match(s);
-        var id = int.Parse(match.Groups["id"].Value);
-        var winning = match.Groups["winning"].Value.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-        var numbers = match.Groups["numbers"].Value.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-        return new(id, winning, numbers);
-    }
+    public int NofWins => winning.Count(numbers.Contains);
 }
 
 static partial class Regexes
 {
     [GeneratedRegex(@"^Card +(?<id>\d.*): +(?<winning>[\d ]+) \| +(?<numbers>.*)")]
-    public static partial Regex MyRegex();
+    public static partial Regex CardRegex();
 }
