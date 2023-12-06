a22f7f3b54b2f3d6cf6571618427bf0267733505
Toon Deburchgrave <toon.deburchgrave@doccle.be>
2023-12-06 17:00:31 +0100

    golf: 253 characters, including input parsing

---
+++ day 06/Toon - python golf/day06.py
@@ -0,0 +1,12 @@
+from math import floor as f, sqrt
+u=open("i").readlines()
+y=u[1].split()[1:]
+def c(t,d):
+ k=t*t-4*d
+ x=sqrt(k)/2
+ m=f(t/2+x)-f(t/2-x)
+ return m-1 if(f(x)-x==0)else m
+r=1
+for i,x in enumerate(u[0].split()[1:]):
+ r*=c(int(x),int(y[i]))
+print(r)
