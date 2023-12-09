d5acfc0f97f57f652d626e6f3b7c7e2f3971bafc
Dante De Ruwe <dantederuwe@gmail.com>
2023-12-09 17:02:35 +0100

    Fix indent size

--- day 04/Dante - C#/DayFourPartTwo.cs
+++ day 04/Dante - C#/DayFourPartTwo.cs
@@ -6,35 +6,35 @@ const StringSplitOptions SplitOptions = StringSplitOptions.TrimEntries | StringS
 
 record Card(int Id, HashSet<int> WinningNumbers, HashSet<int> Numbers)
 {
-		public int CountOfMatchingNumbers => Numbers.Count(WinningNumbers.Contains);
+	public int CountOfMatchingNumbers => Numbers.Count(WinningNumbers.Contains);
 };
 
 static int GetResult(IEnumerable<string> data)
 {
-		var originalCards = data.Select(ParseCard).ToDictionary(x => x.Id, x => x);
-		var cards = new Queue<Card>(originalCards.Values);
+	var originalCards = data.Select(ParseCard).ToDictionary(x => x.Id, x => x);
+	var cards = new Queue<Card>(originalCards.Values);
 
-		var result = 0;
-		
-		while (cards.Count != 0)
-		{
-				result++;
-				var card = cards.Dequeue();
-				var newCardIds = Enumerable.Range(card.Id + 1, card.CountOfMatchingNumbers);
-				foreach (var id in newCardIds) cards.Enqueue(originalCards[id]);
-		}
+	var result = 0;
 
-		return result;
+	while (cards.Count != 0)
+	{
+        result++;
+        var card = cards.Dequeue();
+        var newCardIds = Enumerable.Range(card.Id + 1, card.CountOfMatchingNumbers);
+        foreach (var id in newCardIds) cards.Enqueue(originalCards[id]);
+	}
+
+	return result;
 }
 
 static Card ParseCard(string entry)
 {
-		var split = entry.Split(':', SplitOptions);
-		var cardNumber = int.Parse(split.First().Replace("Card ", string.Empty));
-		
-		var secondSplit = split.Last().Split('|', SplitOptions);
-		var winningNumbers = secondSplit.First().Split(' ', SplitOptions).Select(int.Parse).ToHashSet();
-		var numbers = secondSplit.Last().Split(' ', SplitOptions).Select(int.Parse).ToHashSet();
+	var split = entry.Split(':', SplitOptions);
+	var cardNumber = int.Parse(split.First().Replace("Card ", string.Empty));
 
-		return new Card(cardNumber, winningNumbers, numbers);
+	var secondSplit = split.Last().Split('|', SplitOptions);
+	var winningNumbers = secondSplit.First().Split(' ', SplitOptions).Select(int.Parse).ToHashSet();
+	var numbers = secondSplit.Last().Split(' ', SplitOptions).Select(int.Parse).ToHashSet();
+
+	return new Card(cardNumber, winningNumbers, numbers);
 }
