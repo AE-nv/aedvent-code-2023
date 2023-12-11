ed0ba075786a17f2e3bdb146397efd750624bf37
jhgbrt <jeroen@haegebaert.com>
2023-12-11 20:05:00 +0100

    day 11

---
+++ day 11/Jeroen - C#/Common/TupleUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode
+{
+    static partial class TupleUtils
+    {
+        public static (T a, T b) Sort<T>(this (T a, T b) range) where T : INumber<T> => range.a > range.b ? (range.b, range.a) : range;
+        public static bool Contains<T>(this (T a, T b) range, T n) where T : INumber<T> 
+        {
+            var (a, b) = range.Sort();
+            return a <= n && b >= n;
+        }
+    }
+}
