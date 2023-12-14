37f2956fba4b2e47ebb827f8af6f7ffbc4a003db
Toon Deburchgrave <toon.deburchgrave@doccle.be>
2023-12-14 20:37:33 +0100

    day 14

---
+++ day 14/Toon - Java/Data.java
@@ -0,0 +1,16 @@
+public class Data {
+  public static String getTest() {
+    return """
+        O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....
+        """;
+  }
+}
